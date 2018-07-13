f9b2a2d5099a7441de6e1f1d6472e29c5600139a
Truman Larson <tlarson@silvertech.local>
2018-07-13 16:18:35 -0400

    self.lock() testing

--- __init__.py
+++ __init__.py
@@ -45,10 +45,10 @@ class MySkill(MycroftSkill):
 	        #    dialogs/en-us/hello.world.dialogd
 			
 	        	while true:
-				delay  = datetime.now() + timedelta(seconds = 30)
-		        self.speak_dialog("hello.world")
-				while datetime.now() + delay:
-					#nothing
+                delay  = datetime.now() + timedelta(seconds = 30)
+                self.speak_dialog("hello.world")
+                while datetime.now() + delay:
+                    #nothing
     # The "stop" method defines what Mycroft does when told to stop during
     # the skill's execution. In this case, since the skill's fhiunctionality
     # is extremely simple, there is no need to override it.  If you DO
