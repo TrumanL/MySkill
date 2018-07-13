417f0279cb44a9c17d87d124d2712909a32691bb
Truman Larson <tlarson@silvertech.local>
2018-07-13 17:00:58 -0400

    Updated greetings

--- __init__.py
+++ __init__.py
@@ -49,17 +49,17 @@ class MySkill(MycroftSkill):
 	      self.speak_dialog("hello.world")
 	      while datetime.now() + delay:
 	        #nothing
-  # The "stop" method defines what Mycroft does when told to stop during
-  # the skill's execution. In this case, since the skill's fhiunctionality
-  # is extremely simple, there is no need to override it.  If you DO
-  # need to implement stop, you should return True to indicate you handled
-  # it.
-  #
-  # def stop(self):
-  #    return False
+	  # The "stop" method defines what Mycroft does when told to stop during
+	  # the skill's execution. In this case, since the skill's fhiunctionality
+	  # is extremely simple, there is no need to override it.  If you DO
+	  # need to implement stop, you should return True to indicate you handled
+	  # it.
+	  #
+	  # def stop(self):
+	  #    return False
 
-  # The "create_skill()" method is used to create an instance of the skill.
-  # Note that it's outside the class itself.a
+	  # The "create_skill()" method is used to create an instance of the skill.
+	  # Note that it's outside the class itself.a
 	def create_skill():
 	  return MySkill()
 
