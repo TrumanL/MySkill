1c032b2a051d5753550d7fdb8c8162390924912a
Truman Larson <tlarson@silvertech.local>
2018-07-16 12:18:35 -0400

    Message Queue Testing

--- __init__.py
+++ __init__.py
@@ -40,7 +40,7 @@ class MySkill(MycroftSkill):
   #   'Greetings planet earth'
   @intent_handler(IntentBuilder("").require("Hello").require("World"))  
   def handle_hello_world_intent(self, message):
-  	with open("mycroft-core/messageQueue.json", 'r') as f:
+  	with self.open("/home/truman/Documents/messageQueue.json", 'r') as f:
 	  	messageData = json.load(f)
   	if messageData["messages"].length() > 0:
 	  	for i in messageData["messages"]:
