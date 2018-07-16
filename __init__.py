6bde8b8662ca251742f0f813e4d80d11744df67f
Truman Larson <tlarson@silvertech.local>
2018-07-16 15:19:36 -0400

    Message Queue Testing

--- __init__.py
+++ __init__.py
@@ -61,6 +61,7 @@ class MySkill(MycroftSkill):
                 if any(word in outMessageConfirm for word in yes_words):
                     #outMessage = self.get_response('ask.for_message')
                     #write outMessage to out file
+                    print("")
               open("/home/truman/Documents/messageQueue.json", "w").write(json.dumps(f, sort_keys=True, indent=4, separators=(',', ': ')))
               self.speak("Next Message")
         
