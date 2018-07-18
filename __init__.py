2b3af39aee9b11250722547404096337d1b1e3f5
Truman Larson <tlarson@silvertech.local>
2018-07-18 12:26:53 -0400

    Message Queue Testing

--- __init__.py
+++ __init__.py
@@ -16,6 +16,7 @@ from datetime import datetime, timedelta
 from mycroft.audio import wait_while_speaking
 from mycroft.configuration import ConfigurationManager
 from mycroft.messagebus.client.ws import WebsocketClient
+from websocket import create_connection
 
 # Each skill is contained within its own class, which inherits base methods
 # from the MycroftSkill class.  You extend this class as shown below.
