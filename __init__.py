bcb3026bd1c32146fd8ba3c6611fbcd8034ffac1
Truman Larson <tlarson@silvertech.local>
2018-07-23 16:34:58 -0400

    File system fixing

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ from mycroft.configuration import ConfigurationManager
 from os.path import join, abspath, dirname
 from mycroft.filesystem import FileSystemAccess
 try:
-    import GPIO as GPIO
+    import RPi.GPIO as GPIO
 except:
     print(sys.exc_info())
 
