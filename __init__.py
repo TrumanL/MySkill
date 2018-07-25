abc789faae4ef2643953b39ac5691b21af42c9f7
Truman Larson <tlarson@silvertech.local>
2018-07-25 11:12:08 -0400

    File system fixing

--- __init__.py
+++ __init__.py
@@ -36,9 +36,9 @@ class MySkill(MycroftSkill):
       
       try:
           GPIO.setmode(GPIO.BCM)
-          GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+          GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
           self.log.info("******GPIO SETUP")
-          GPIO.add_event_detect(23, GPIO.FALLING, callback=self.handle_read_messages_passive, bouncetime=300)
+          GPIO.add_event_detect(17, GPIO.FALLING, callback=self.handle_read_messages_passive, bouncetime=300)
           self.log.info("******GPIO EVENT ADDED")
       except:
           self.log.info("******GPIO EVENT FAILED")
