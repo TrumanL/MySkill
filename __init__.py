1b00ad54007dae60a72550f7c739f8e85aa1eb5c
Truman Larson <tlarson@silvertech.local>
2018-07-30 11:26:51 -0400

    Audio Record Testing

--- __init__.py
+++ __init__.py
@@ -121,7 +121,7 @@ class MySkill(MycroftSkill):
                     wait_while_speaking()
                     #record(self.file_system.path+'/test.wav', 600, 44100, 1)
                     self.log.info("Audio Request Sending")
-                    self.emitTest.emit(Message.deserialize(json.dumps({"type":"skill-audio-record:AudioRecordSkillIntent", "data":{"utterance":"record audio 10 seconds"}})))
+                    self.emitTest.wait_for_response(Message.deserialize(json.dumps({"type":"skill-audio-record:AudioRecordSkillIntent", "data":{"utterance":"record audio 10 seconds"}})), 'mycroft.skill.handler.complete', 10)
                     time.sleep(10)
                     self.speak('Done')
                     wait_while_speaking()
